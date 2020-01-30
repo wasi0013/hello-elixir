03c496ee338fc4ff830acedf20c34d3c44c4f656
Wasi Mohammed Abdullah <wasi0013@gmail.com>
2020-01-30 17:53:43 +0600

    Solved Day 1 2019

---
+++ advent_of_code/2019/day_1/solution.ex
@@ -0,0 +1,34 @@
+defmodule Solution do
+  def run do
+    solve_part_1()
+    solve_part_2()
+  end
+  
+  def solve_part_1 do
+    get_input()
+    |> Enum.map(&get_fuel_req/1)
+    |> Enum.sum
+    |> IO.puts
+  end
+
+  def solve_part_2 do
+    get_input()
+    |> Enum.map(&get_added_fuel/1)
+    |> Enum.sum
+    |> IO.puts
+  end
+
+  def get_input do 
+    {:ok, data} = File.read("input.txt")
+    String.split(data, ["\n"])
+    |> Enum.filter(fn (item) -> item != "" end)
+    |> Enum.map(&String.to_integer/1)
+  end
+
+  def get_fuel_req(mass), do: div(mass, 3) - 2
+
+  def get_added_fuel(mass) do
+    value = get_fuel_req(mass)
+    if (value > 0), do:  value + get_added_fuel(value), else: 0
+  end
+end
